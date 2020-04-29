008c88cb04aa7f3fc9a72019bfe9757f8fd1739b
Abhishek Mamdapure <47754293+abhishekmamdapure@users.noreply.github.com>
2020-04-29 14:11:59 +0530

    adding UVA_numeric(Uni Variate analysis on data)

--- imp_functions.py
+++ imp_functions.py
@@ -114,5 +114,51 @@ def zscore_outliers(data):
     return data_outlier.shape
 
 
+####################################################################################################
+
+# Python Code to Understand Normal Distribution
+# copyright from Analytics Vidhya
+
+def UVA_numeric(data):
+    var_group = data.columns
+    size = len(var_group)
+    plt.figure(figsize = (7*size,3), dpi = 400)
+
+    #looping for each variable
+    for j,i in enumerate(var_group):
+        
+
+        # calculating descriptives of variable
+        mini = data[i].min()
+        maxi = data[i].max()
+        ran = data[i].max()-data[i].min()
+        mean = data[i].mean()
+        median = data[i].median()
+        st_dev = data[i].std()
+        skew = data[i].skew()
+        kurt = data[i].kurtosis()
+
+        # calculating points of standard deviation
+        points = mean-st_dev, mean+st_dev
+
+        #Plotting the variable with every information
+        plt.subplot(1,size,j+1)
+        sns.distplot(data[i],hist=True, kde=True)
+        
+        sns.lineplot(points, [0,0], color = 'black', label = "std_dev")
+        sns.scatterplot([mini,maxi], [0,0], color = 'orange', label = "min/max")
+        sns.scatterplot([mean], [0], color = 'red', label = "mean")
+        sns.scatterplot([median], [0], color = 'blue', label = "median")
+        plt.xlabel('{}'.format(i), fontsize = 20)
+        plt.ylabel('density')
+        plt.title('std_dev = {}; kurtosis = {};\nskew = {}; range = {}\nmean = {}; median = {}'.format((round(points[0],2),round(points[1],2)),
+                                                                                                       round(kurt,2),
+                                                                                                       round(skew,2),
+                                                                                                       (round(mini,2),round(maxi,2),round(ran,2)),
+                                                                                                       round(mean,2),
+                                                                                                       round(median,2)))
+        
+ ##################################################################################################################
+
 
 
